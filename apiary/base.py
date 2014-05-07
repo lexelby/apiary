dec2a7c53d00839e8e27d815930a6a1edbbb9534
Lex <lex@deviantart.com>
2014-05-06 17:36:07 -0700

    various fixes to make threading work

--- apiary/base.py
+++ apiary/base.py
@@ -50,6 +50,7 @@ from multiprocessing import Value
 import rabbitpy
 
 from apiary.tools.childprocess import ChildProcess
+from apiary.tools.transport import Transport, ConnectionError
 from apiary.tools.debug import debug, traced_func, traced_method
 
 verbose = False
@@ -71,16 +72,23 @@ class BeeKeeper(object):
     def start(self):
         """Run the load test."""
 
+        clean(self.options)
+
         start_time = time.time()
 
         workers = []
 
+        delay = self.options.stagger_workers / 1000.0
         for i in xrange(self.options.workers):
             worker = WorkerBeeProcess(self.options, self.protocol)
             worker.start()
             workers.append(worker)
+            time.sleep(delay)
 
         # TODO: consider waiting until workers are ready
+        #if self.options.startup_wait:
+        #    print "workers started; waiting %d seconds..." % self.options.startup_wait
+        #    time.sleep(self.options.startup_wait)
 
         stats_gatherer = StatsGatherer(self.options)
         stats_gatherer.start()
@@ -103,7 +111,7 @@ class BeeKeeper(object):
             transport.connect()
             transport.queue('worker-job', clean=False)
 
-            for worker in workers:
+            for worker in xrange(self.options.workers * self.options.threads):
                 transport.send('worker-job', cPickle.dumps(Message(Message.STOP_WORKER)))
 
             # Now wait for the workers to get the message.  This may take a few
@@ -207,7 +215,7 @@ class QueenBee(ChildProcess):
         transport.connect()
         transport.queue('worker-job', clean=True)
 
-        start_time = time.time()
+        start_time = time.time() + self._options.startup_wait
 
         sequence_file = open(self._sequence_file, 'rb')
 
@@ -236,7 +244,7 @@ class QueenBee(ChildProcess):
                             print "WARNING: Queenbee is %0.2f seconds behind." % (-offset)
                             self._last_warning = time.time()
 
-                message = Message(Message.JOB, job)
+                message = Message(Message.JOB, (start_time, job))
                 message = cPickle.dumps(message)
                 transport.send('worker-job', message)
             except EOFError:
@@ -259,13 +267,15 @@ class WorkerBee(Thread):
         self.verbose = options.verbose >= 1
         self.debug = options.debug
         self.time_scale = 1.0 / options.speedup
-        self.start_time = time.time()
+        self.start_time = None
 
     def status(self, status, body=None):
+        #print 'status:', status, body
         message = rabbitpy.Message(self.channel, cPickle.dumps(Message(status, body)))
-        message.publish(self.exchange, 'worker-job')
+        message.publish(self.exchange, 'worker-status')
 
     def error(self, body):
+        print 'error:', body
         self.status(Message.JOB_ERROR, body)
 
     def process_message(self, msg):
@@ -277,30 +287,39 @@ class WorkerBee(Thread):
             # Jobs look like this:
             # (job_id, ((time, request), (time, request), ...))
 
-            job_id, tasks = message.body
+            if self.start_time is None:
+                #print "worker starting first job"
+                self.start_time = time.time()
 
+            start_time, (job_id, tasks) = message.body
             self.status(Message.JOB_STARTED)
 
             if self.dry_run or not tasks:
                 self.status(Message.JOB_COMPLETED)
                 return
 
-            self.start_job(job_id)
-
+            started = False
             error = False
 
             for timestamp, request in tasks:
-                target_time = timestamp * self.time_scale + self.start_time
+                target_time = timestamp * self.time_scale + start_time
                 offset = target_time - time.time()
 
                 # TODO: warn if falling behind?
 
                 if offset > 0:
+                    #print('sleeping %0.4f seconds' % offset)
                     debug('sleeping %0.4f seconds' % offset)
                     if offset > 120 and self.verbose:
                         print "long wait of %ds for job %s" % (offset, job_id)
                     time.sleep(offset)
+                #elif offset < -1:
+                #    print "worker fell behind by %.5f seconds" % (-offset)
 
+                if not started:
+                    self.start_job(job_id)
+
+                #print "sending request", request
                 error = not self.send_request(request)
                 if error:
                     break
@@ -326,12 +345,20 @@ class WorkerBee(Thread):
                                    auto_delete=False)
         self.job_queue.declare()
 
+        self.status_queue = rabbitpy.Queue(self.channel,
+                                   'worker-status',
+                                   durable=False,
+                                   auto_delete=False)
+        self.status_queue.declare()
+
         self.exchange = rabbitpy.Exchange(self.channel, self.EXCHANGE)
         self.exchange.declare()
 
-        self.job_queue.bind(exchange, 'worker-job')
+        self.job_queue.bind(self.exchange, 'worker-job')
+        self.status_queue.bind(self.exchange, 'worker-status')
 
         for message in self.job_queue.consume_messages(prefetch=1):
+            #print "got message"
             done = self.process_message(message)
             message.ack()
 
@@ -357,13 +384,17 @@ class WorkerBeeProcess(ChildProcess):
              self.options.amqp_vhost)
 
         with rabbitpy.Connection(url) as conn:
+            delay = self.options.stagger_threads / 1000.0
             for i in xrange(self.options.threads):
                 thread = self.protocol.WorkerBee(self.options, conn.channel())
                 thread.setDaemon(True)
                 thread.start()
                 self.threads.append(thread)
+                time.sleep(delay)
 
-            for thread in threads:
+            print "spawned %d threads" % len(self.threads)
+
+            for thread in self.threads:
                 thread.join()
 
         print 'worker ended'
@@ -413,9 +444,18 @@ def add_options(parser):
     parser.add_option('-w', '--workers', metavar='N',
                       default=100, type='int',
                       help='number of worker bee processes (default: 100)')
+    parser.add_option('--stagger-workers', metavar='MSEC',
+                      default=0, type='int',
+                      help='number of milliseconds to wait between starting workers (default: 0)')
     parser.add_option('-t', '--threads', metavar='N',
                       default=1, type='int',
                       help='number of threads per worker process (default: 1)')
+    parser.add_option('--stagger-threads', metavar='MSEC',
+                      default=0, type='int',
+                      help='number of milliseconds to wait between starting threadss (default: 0)')
+    parser.add_option('--startup-wait', metavar='SEC',
+                      default=0, type='int',
+                      help='number of seconds to wait after starting all workers before enqueuing jobs (default: 0)')
     parser.add_option('--clean',
                       action='store_true', default=False,
                       help='clean up all queues')
